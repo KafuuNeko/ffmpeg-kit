9952f67e017d51f4ea151412c683eb2e388257a8
Kafuu Neko <kafuuneko@gmail.com>
2024-05-14 03:23:33 +0800

    Create build-bvd-ffmpeg.sh

---
+++ build-bvd-ffmpeg.sh
@@ -0,0 +1,5 @@
+export JAVA_HOME=/home/kafuu/Java/jdk-17.0.10
+export ANDROID_SDK_ROOT=/home/kafuu/Android/sdk
+export ANDROID_NDK_ROOT=/home/kafuu/Android/android-ndk-r22b
+
+./android.sh --disable-arm-v7a-neon --enable-gpl --enable-x264 --enable-x265 --enable-android-media-codec --enable-android-zlib   --enable-dav1d --enable-kvazaar --enable-lame  --enable-libaom --enable-libilbc --enable-libtheora --enable-libvorbis --enable-libvpx --enable-libwebp --enable-openh264 --enable-opus --enable-shine --enable-snappy --enable-soxr --enable-speex --enable-twolame --enable-vo-amrwbenc --enable-zimg
